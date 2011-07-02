8c82780a6f98ac171567147e4fb65ea253c81046
Sascha Peilicke <saschpe@gmx.de>
2011-07-02 16:39:11 +0200

    Added doc.go file

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright 2011 Sascha Peilicke. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+	Package tribool implements a ternary (three-valued) logic and
+	a corresponding type 'Tribool'.
+*/
+package tribool
