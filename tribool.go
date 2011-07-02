79cb404a63603b96a9f09e4eae7b2a50fa825e73
Sascha Peilicke <saschpe@gmx.de>
2011-07-02 15:33:36 +0200

    Use simple implementation

--- tribool.go
+++ tribool.go
@@ -1,96 +1,59 @@
 package tribool
 
-type Tribool interface {
-    True() bool
-    Indeterminate() bool
-    False() bool
-
-    Not() Tribool
-    And(b Tribool) Tribool
-    Or(b Tribool) Tribool
-}
-
-type tribool struct {
-    value int
-}
-
-const tribool_true = 2
-const tribool_indeterminate = 1
-const tribool_false = 0
+type Tribool int
+const True = 2
+const Indeterminate = 1
+const False = 0
 
 func New(value string) Tribool {
-    var ret tribool
+    ret := Tribool(False)
     switch value {
-        case "true": ret.value = tribool_true
-        case "indeterminate": ret.value = tribool_indeterminate
-        case "false": ret.value = tribool_false
-    }
-    return &ret
-}
-
-func (a *tribool) True() bool { return a.value == tribool_true }
-func (a *tribool) Indeterminate() bool { return a.value == tribool_indeterminate }
-func (a *tribool) False() bool { return a.value == tribool_false }
-
-func (a *tribool) Not() Tribool {
-    ret := a
-    switch ret.value {
-        case tribool_true: ret.value = tribool_false
-        case tribool_false: ret.value = tribool_true
+        case "true": ret = True
+	case "indeterminate": ret = Indeterminate
     }
     return ret
 }
 
-func (a *tribool) And(b Tribool) Tribool {
-    var ret tribool
-    if a.True() {
-        if b.True() {
-            ret.value = tribool_true
-        } else if b.Indeterminate() {
-            ret.value = tribool_indeterminate
-        } else {
-            ret.value = tribool_false
-        }
-    } else if a.Indeterminate() {
-        if b.False() {
-            ret.value = tribool_false
-        } else {
-            ret.value = tribool_indeterminate
-        }
-    } else { // a.False()
-        ret.value = tribool_false
+func (a Tribool) True() bool { return a == True }
+func (a Tribool) Indeterminate() bool { return a == Indeterminate }
+func (a Tribool) False() bool { return a == False }
+
+func (a Tribool) Not() Tribool {
+    switch a {
+	case True: return False
+	case False: return True
     }
-    return &ret
+    return Indeterminate
 }
 
-func (a *tribool) Or(b Tribool) Tribool {
-    var ret tribool
-    if a.True() {
-        ret.value = tribool_true
-    } else if a.Indeterminate() {
-        if b.True() {
-            ret.value = tribool_true
-        } else {
-            ret.value = tribool_indeterminate
-        }
-    } else { // a.False()
-        if b.True() {
-            ret.value = tribool_true
-        } else if b.Indeterminate() {
-            ret.value = tribool_indeterminate
-        } else {
-            ret.value = tribool_false
-        }
+func min(a, b int) int {
+    if a < b {
+	return a
     }
-    return &ret
+    return b
 }
 
-func (a tribool) String() string {
-    var ret string
-    switch a.value {
-        case tribool_true: ret = "true"
-        case tribool_indeterminate: ret = "indeterminate"
-        case tribool_false: ret = "false"
+func max(b, a int) int {
+    if a > b {
+	return a
+    }
+    return b
+}
+
+func (a Tribool) And(b Tribool) Tribool {
+    return Tribool(min(int(a), int(b)))
+}
+
+func (a Tribool) Or(b Tribool) Tribool {
+    return Tribool(max(int(a), int(b)))
+}
+
+func (a Tribool) String() string {
+    ret := "indeterminate"
+    switch a {
+        case True: ret = "true"
+        case False: ret = "false"
     }
     return ret
 }
+
